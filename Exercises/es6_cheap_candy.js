38660ce4caf77f7125f871419cfb0eca404701a0
Sasha McBride <mcbride.sasha@gmail.com>
2023-05-18 15:47:57 -0400

    initial

---
+++ Exercises/es6_cheap_candy.js
@@ -0,0 +1,12 @@
+let products = [
+    {product: "Gummy Worms", price: 5.79},
+    {product: "Plain M&Ms", price: 2.89},
+    {product: "Peanut M&Ms", price: 2.89},
+    {product: "Swedish Fish", price: 3.79},
+    {product: "Swiss Rolls", price: 1.79},
+    {product: "Reese Cups", price: 1.89},
+    {product: "FastBreak", price: 1.65},
+    {product: "Honey Bun", price: 1.99}
+    // TODO: fill the array with 10 candies of various
+    // price ranges
+]; nhkmb --
